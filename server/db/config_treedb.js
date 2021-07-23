d884375f819e74741e5547a5e194cf7e382d9f2e
Jason Zhen <jasonzhen.mail@gmail.com>
2021-07-22 18:31:58 -0700

    Fix morgan logger format and treedb config

--- server/db/config_treedb.js
+++ server/db/config_treedb.js
@@ -1,66 +1,15 @@
 const env = process.argv[2] || 'local';
 
 const configTreeDB = {
-    connectionLimit: 10, // what does this mean?
-    host: process.env.DATABASE_HOST || 'postgis-wtt',
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT || 5432,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    dateStrings: 'date',
-    connect_timeout: 10,
-  },
-  dockercl: {
-    connectionLimit: 10, // what does this mean?
-    database: 'treedb',
-    user: 'trees',
-    host: '127.0.0.1',
-    password: 'trees3r4t',
-    port: 5432,
-    dateStrings: 'date',
-    connect_timeout: 10,
-  },
-  local: {
-    connectionLimit: 10, // what does this mean?
-    database: 'treedb',
-    user: 'trees',
-    host: 'localhost',
-    password: 'trees3r4t',
-    port: 5432,
-    dateStrings: 'date',
-    connect_timeout: 10,
-  },
-  dev: {
-    connectionLimit: 10, // what does this mean?
-    database: 'treedb',
-    user: 'trees',
-    host: 'localhost',
-    password: 'trees3r4t',
-    port: 5432,
-    dateStrings: 'date',
-    connect_timeout: 10,
-  },
-  blue: {
-    connectionLimit: 10, // what does this mean?
-    database: 'treedbblue',
-    user: 'trees',
-    host: 'localhost',
-    password: 'trees3r4t',
-    port: 5432,
-    dateStrings: 'date',
-    connect_timeout: 10,
-  },
-  prod: {
-    connectionLimit: 10, // what does this mean?
-    database: 'treedb',
-    user: 'trees',
-    host: 'localhost',
-    password: 'trees3r4t',
-    port: 5432,
-    dateStrings: 'date',
-    connect_timeout: 10,
-  },
-}[env];
+  connectionLimit: 10, // what does this mean?
+  host: process.env.DATABASE_HOST || 'postgis-wtt',
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT || 5432,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  dateStrings: 'date',
+  connect_timeout: 10,
+};
 
 const configPgNative = `
   host=${configTreeDB.host}
@@ -68,7 +17,8 @@ const configPgNative = `
   user=${configTreeDB.user}
   password=${configTreeDB.password}
   port=${configTreeDB.port}
-  connect_timeout=${configTreeDB.connect_timeout}`;
+  connect_timeout=${configTreeDB.connect_timeout}
+`;
 
 //console.log(configTreeDB,'configTreeDB', ' configPgNative', configPgNative);
 module.exports = { configTreeDB, configPgNative };

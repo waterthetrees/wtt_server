6ae5f8c63505b89b4208c0725df1d1ff83655d55
Anthony Chian <anthonychian@gmail.com>
2021-07-26 18:23:22 -0700

    updated find user tree history query

--- server/models/user-tree-history.js
+++ server/models/user-tree-history.js
@@ -2,10 +2,9 @@ const { queryTreeDB } = require('../db/treedb.js');
 
 function findUserTreeHistory(volunteer) {
   const query = `
-    SELECT id_treehistory as "idTreeHistory", id_tree AS "idTree",
-    watered, mulched, weeded, staked, braced, pruned, liked, adopted,
-    date_visit as "dateVisit", comment, volunteer
-    FROM treehistory WHERE volunteer = 'rommims';
+    SELECT date_visit as "dateVisit", liked, adopted, watered, mulched, weeded, staked, braced, pruned
+    FROM treehistory
+    WHERE volunteer = 'rommims';
   `;
 
   return queryTreeDB(query, findUserTreeHistory.name);

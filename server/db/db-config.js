5f02dc6a1a1c82f1a5ba0190d1e0ba6fb02cde5b
zoobot <rommims@gmail.com>
2022-02-24 23:39:53 -0800

    config

--- server/db/db-config.js
+++ server/db/db-config.js
@@ -1,11 +1,14 @@
 /**
  * https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax
  */
+import dotenv from "dotenv";
+dotenv.config();
+ 
 const dbConfig = {
-  host: process.env.POSTGRES_HOST || 'wtt_postgis',
-  port: process.env.POSTGRES_PORT || 5432,
-  database: process.env.POSTGRES_DB || 'treedb',
-  user: process.env.POSTGRES_USER || 'trees',
-  password: process.env.POSTGRES_PASSWORD || 'trees3r4t',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
 };
 export default dbConfig
